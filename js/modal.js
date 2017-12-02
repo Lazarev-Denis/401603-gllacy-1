e481101e8f17f1357baeaf7c99fe65b00728b986
Lazarev Denis <30746129+Lazarev-Denis@users.noreply.github.com>
2017-12-02 10:30:53 +0200

    Еще финальная доработка.

---
+++ js/modal.js
@@ -0,0 +1,51 @@
+		var link = document.querySelector(".button-feedback");
+
+		var popup = document.querySelector(".modal-feedback");
+		var close = popup.querySelector(".modal-close");
+
+		var form = popup.querySelector("form");
+		var login = popup.querySelector("[name=name]");
+		var email = popup.querySelector("[name=email]");
+		var text = popup.querySelector("[name=text");
+
+		var storage = localStorage.getItem("login");
+
+		link.addEventListener("click", function (evt) {
+			evt.preventDefault();
+			popup.classList.add("modal-show");
+
+			if (storage) {
+				login.value = storage;
+				email.focus();
+			} else {
+				login.focus();
+			}
+		});
+
+		close.addEventListener("click", function (evt){
+			evt.preventDefault();
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+		});
+
+		form.addEventListener("submit", function (evt) 
+			{
+			if (!login.value || !email.value || !text.value) {
+				evt.preventDefault();
+				popup.classList.remove("modal-error");
+      			popup.offsetWidth = popup.offsetWidth;
+				popup.classList.add("modal-eror");
+			} else {
+				localStorage.setItem("login", login.value);
+			}
+		});
+
+		  window.addEventListener("keydown", function (evt) {
+    		if (evt.keyCode === 27) {
+      			if (popup.classList.contains("modal-show")) 
+      			{
+        		popup.classList.remove("modal-show");
+        		popup.classList.remove("modal-error");
+      			}
+    		}
+  		});
